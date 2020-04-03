91a9f668c2191c9d4ee5c35d77b4328ef5a788df
Frizlab <fload@me.com>
2020-04-03 19:13:47 +0200

    List files and folders in a folder

--- Sources/SwiftWebLsKit/Controllers/FileAndFolderController.swift
+++ Sources/SwiftWebLsKit/Controllers/FileAndFolderController.swift
@@ -12,7 +12,7 @@ public final class FileAndFolderController {
 		publicDirectory = URL(fileURLWithPath: d, isDirectory: true)
 	}
 	
-	public func serveFileOrDirectory(_ request: Request) -> EventLoopFuture<Response> {
+	public func serveFileOrDirectory(_ request: Request) throws -> EventLoopFuture<Response> {
 		var path = request.url.path
 		
 		/* path must be relative */
@@ -20,9 +20,14 @@ public final class FileAndFolderController {
 			path = String(path.dropFirst())
 		}
 		
+		/* We also remove the trailing slashes (for cosmetic purpose) */
+		while path.hasSuffix("/") {
+			path = String(path.dropLast())
+		}
+		
 		/* Protect against relative paths */
 		guard !path.contains("../") else {
-			return request.eventLoop.makeFailedFuture(Abort(.forbidden))
+			throw Abort(.forbidden)
 		}
 		
 		/* Create absolute file path URL */
@@ -30,15 +35,49 @@ public final class FileAndFolderController {
 		
 		/* Check if file exists and is not a directory */
 		var isDir: ObjCBool = false
-		guard FileManager.default.fileExists(atPath: fileURL.path, isDirectory: &isDir), !isDir.boolValue else {
-			return request.view.render("folder").flatMap{ $0.encodeResponse(for: request) }
+		guard FileManager.default.fileExists(atPath: fileURL.path, isDirectory: &isDir) else {
+			throw Abort(.notFound)
 		}
 		
-		/* Stream the file */
-		let res = request.fileio.streamFile(at: fileURL.path)
-		res.headers.contentType = .binary
-		res.headers.contentDisposition = .init(.attachment, name: fileURL.deletingPathExtension().lastPathComponent, filename: fileURL.lastPathComponent)
-		return request.eventLoop.makeSucceededFuture(res)
+		if isDir.boolValue {
+			struct FolderContext : Encodable {
+				var folderName: String?
+				var folderPath: String
+				var subFolderNames: [String]
+				var fileNames: [String]
+			}
+			
+			var context = FolderContext(
+				folderName: path.split(separator: "/").last.flatMap(String.init),
+				folderPath: "/" + path,
+				subFolderNames: [],
+				fileNames: []
+			)
+			
+			/* List files and folders in the folder */
+			let elts = try FileManager.default.contentsOfDirectory(at: fileURL, includingPropertiesForKeys: [.isDirectoryKey])
+			for u in elts {
+				/* Filter out hidden files */
+				guard !u.lastPathComponent.hasPrefix(".") else {continue}
+				
+				guard let isDir = try u.resourceValues(forKeys: [.isDirectoryKey]).isDirectory else {
+					throw Abort(.internalServerError)
+				}
+				if isDir {
+					context.subFolderNames.append(u.lastPathComponent)
+				} else {
+					context.fileNames.append(u.lastPathComponent)
+				}
+			}
+			
+			return request.view.render("folder", context).flatMap{ $0.encodeResponse(for: request) }
+		} else {
+			/* Stream the file */
+			let res = request.fileio.streamFile(at: fileURL.path)
+			res.headers.contentType = .binary
+			res.headers.contentDisposition = .init(.attachment, name: fileURL.deletingPathExtension().lastPathComponent, filename: fileURL.lastPathComponent)
+			return request.eventLoop.makeSucceededFuture(res)
+		}
 	}
 	
 }
