02ffcbbb8ba16cc051c86db29c4a23aed736e58d
Frizlab <fload@me.com>
2020-04-04 13:03:58 +0200

    Implement file uploads

--- Sources/SwiftWebLsKit/routes.swift
+++ Sources/SwiftWebLsKit/routes.swift
@@ -6,6 +6,8 @@ import Vapor
 
 func routes(_ app: Application) throws {
 	let c = FileAndFolderController(publicDirectory: app.directory.publicDirectory)
-	app.get(use: c.serveFileOrDirectory)
+	app.get(use: c.serveFileOrDirectory) /* Probably needed because of a bug: https://github.com/vapor/vapor/issues/2288 */
 	app.get(.catchall, use: c.serveFileOrDirectory)
+	app.post(use: c.receiveFile)
+	app.post(.catchall, use: c.receiveFile)
 }
